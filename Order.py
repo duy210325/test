066a2e700c3072cb3de1c78d0f499fc9ad7f4f60
Duy <nguyenkhanhduy875@gmail.com>
2025-09-01 11:11:48 +0700

    update

--- Order.py
+++ Order.py
@@ -1,11 +1,11 @@
 class Item:
-    def __init__(self, id, name, price):
-        self.id = id
+    def __init__(self, name, amount, price):
+        self.amount = amount
         self.name = name
-        self.price = price
+        self.price =  price
 
     def __str__(self):
-        return f"{self.id, self.name, self.price }"
+        return f"{self.name, self.amount, self.price }"
     
 class Node:
     def __init__(self, data):
@@ -16,13 +16,15 @@ class dataList:
     def __init__(self):
         self.head = self.tail = None
     
-    def enqueue(self, id, name, price):
-        new_node = Node(Item(id, name, price =0 ))
+
+    def addLast(self, name , amount, price):
+        new_node = Node(Item(name, amount, price))
         if self.head == None:
             self.head = self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
+
     def display(self):
         print("Data List:")
         if self.head is None:
@@ -32,11 +34,24 @@ class dataList:
             print(current.data)
             current = current.next
             print('==============')
+
     def loadData(self, file_path, size):
         data = read_file(file_path)[0]
         for i in range(size):
             self.enqueue(data)[3*i], data[3*i+1], data[3*i+2]
+
 class RequestQueue: 
+    def __init__(self):
+        self.head = self.tail = None
+    
+    def enqueue(self, id, name, price):
+        new_node = Node(Item(id, name, price =0 ))
+        if self.head == None:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
     def dequeue(self):
         tmp = None
         if self.head == None:
@@ -45,9 +60,24 @@ class RequestQueue:
         tmp = self.head
         self.head = self.head.next
         return tmp
+    
+    def display(self):
+        print("Request Queue: ")
+        if self.head is None:
+            print("Empty")
+        ccur = self.head
+        while ccur:
+            print(ccur.data.name) + str(",") + str(ccur.data.price)        
+            ccur = ccur.next
+            print('=============')
+    def loadRequest(self, file_path, m,n)
+        data = read_file(file_path)[1]
+        for i in range:
+            self.enqueue(data[2*i], daya[2*i+1])
 class ComputerStore:
     def __init__(self):
-        self.data = dataList()     
+        self.data = dataList()  
+
     def purchase(self,data): 
         money = 0
         current = self.
